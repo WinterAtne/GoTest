17dd6b4c0db52df93afbee175e7ec0499da208d8
WinterAtne <Winter54b@gmail.com>
2023-09-30 22:06:50 -0600

    Added bullet spread

--- bullet.gd
+++ bullet.gd
@@ -1,7 +1,8 @@
 extends Area2D
 const speed = 1600
+const spread : Vector2 = Vector2(-0.25, 0.25)
 
-@onready var direction = position.direction_to(get_global_mouse_position())
+@onready var direction = position.direction_to(get_global_mouse_position()) + Vector2(randf_range(spread.x, spread.y), randf_range(spread.x, spread.y))
 
 
 
