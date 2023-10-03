e9668a5eb96ff20434cc4bf39f91cd9ef25deaa8
WinterAtne <Winter54b@gmail.com>
2023-10-02 18:32:19 -0600

    Changed the time it takes bullets to despawn

--- bullet.gd
+++ bullet.gd
@@ -5,7 +5,7 @@ const spread : Vector2 = Vector2(-0.05, 0.05)
 @onready var direction = position.direction_to(get_global_mouse_position()) + Vector2(randf_range(spread.x, spread.y), randf_range(spread.x, spread.y))
 
 func _ready():
-	await get_tree().create_timer(3).timeout
+	await get_tree().create_timer(1.3).timeout
 	queue_free()
 
 func _process(delta):
